d1a33259a5aeea9edfa3521341e44568651acf53
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-05 18:42:10 +0200

    adding to list

--- DragAndDrop.js
+++ DragAndDrop.js
@@ -40,21 +40,33 @@ function handleFiles(files) {
 
 function uploadFile(file) {
   let textFromFile = "";
-
+  let dataSetName = getFileName(file);
   let reader = new FileReader();
 
   reader.readAsText(file);
   //async problem code executes while file loading so when the console tries
   //to log the text from file it was not completed yet
   //Thus write into onload function
-  console.log("hi 1");
+
   reader.onload = function () {
     textFromFile = reader.result;
-    console.log(typeof reader.result, reader.result);
-    console.log(textFromFile);
+
+    // console.log(typeof reader.result, reader.result);
+    // console.log(textFromFile);
     let paragraph = document.getElementById("textFromFiles");
 
-    console.log(file.name);
+    let dataSetList = document.getElementById("file-list");
+
+    let newDataSet = document.createElement("li");
+    newDataSet.appendChild(document.createTextNode(dataSetName));
+    newDataSet.className = "list-group-item";
+
+    dataSetList.appendChild(newDataSet);
+
+    // console.log(file.name);
+    // console.log(file.type);
+    // console.log(file.size);
+    console.log(dataSetName);
     paragraph.textContent = textFromFile;
   };
 
@@ -63,4 +75,22 @@ function uploadFile(file) {
   };
 
   console.log("hi 2");
+}
+
+function getFileName(file) {
+  if (file.name.includes(".")) {
+    let fileName = "";
+
+    for (let i = 0; i < file.name.length; i++) {
+      const char = file.name[i];
+
+      if (char === ".") break;
+
+      fileName += char;
+    }
+
+    return fileName;
+  }
+
+  return file.name;
 }
