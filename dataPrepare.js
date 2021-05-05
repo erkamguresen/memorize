75271a18a9394fdc2a205f3bbc1a10d9fce2e88c
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-05 22:53:17 +0200

    tokenizer

--- dataPrepare.js
+++ dataPrepare.js
@@ -8,10 +8,25 @@ function prepareData(e) {
     let dataSetName = getDataSetName(e);
 
     //TODO :make objects
+    let dataObjects = getDataObject(dataSetName);
+
+    // let dataJSON = JSON.stringify(dataObject);
 
     //TODO: Save data object to local storage
+    localStorage.setItem(`${dataSetName}Data`, dataObjects);
+    // localStorage.setItem(`${dataSetName}Data`, dataJSON);
 
-    console.log(sessionStorage.getItem(dataSetName));
+    console.log(localStorage.getItem(`${dataSetName}Data`));
+
+    //TODO: Main panel content
+
+    //TODO: H1 data set name
+
+    //TODO: Button for study button
+
+    //TODO: Button for quiz
+
+    // console.log(sessionStorage.getItem(dataSetName));
 
     //     //there is formatting and extra X so use first child which is the only tex node :-)
     //     let text = currentLi.firstChild.textContent.trim();
@@ -32,4 +47,82 @@ function getDataSetName(event) {
   }
 
   return currentElement.firstChild.textContent;
+}
+
+function getDataObject(dataSetName) {
+  let returnArray = [];
+
+  try {
+    let rawData = sessionStorage.getItem(dataSetName);
+    let lines = getLineTokens(rawData);
+
+    console.log(lines);
+  } catch (e) {
+    console.error(e);
+  }
+
+  return returnArray;
+}
+
+function getLineTokens(textToTokenize) {
+  let returnArray = [];
+  let line = "";
+
+  for (let i = 0; i < textToTokenize.length; i++) {
+    let char = textToTokenize[i];
+
+    if (i === textToTokenize.length - 1) {
+      line += char;
+      returnArray.push(line);
+      break;
+    }
+
+    if (char !== "\n") {
+      line += char;
+      continue;
+    } else {
+      // char === "\n"
+      returnArray.push(line);
+
+      line = "";
+    }
+  }
+
+  return returnArray;
+}
+
+function getWordTokens(textLineToTokenize) {
+  let returnArray = [];
+  let word = "";
+
+  for (let i = 0; i < textLineToTokenize.length; i++) {
+    let char = textLineToTokenize[i];
+
+    if (i === textLineToTokenize.length - 1) {
+      if (char !== " " && char !== "" && char !== "\n") word += char;
+
+      returnArray.push(word);
+
+      break;
+    }
+
+    if (
+      char !== "\n" &&
+      char !== "\t" &&
+      char !== " " &&
+      char !== "\r" &&
+      char !== "\f"
+    ) {
+      word += char;
+
+      continue;
+    } else {
+      if (word !== " " && word !== "" && word !== "\n") returnArray.push(word);
+
+      word = "";
+      continue;
+    }
+  }
+
+  return returnArray;
 }
