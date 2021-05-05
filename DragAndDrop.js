77379ac1ea3b38dbd8f6e549015718ca9cc3be84
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-05 17:10:53 +0200

    Working drag and drop for

--- DragAndDrop.js
+++ DragAndDrop.js
@@ -1,101 +1,65 @@
-// ************************ Drag and drop ***************** //
 let dropArea = document.getElementById("drop-area");
 
-// Prevent default drag behaviors
 ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
   dropArea.addEventListener(eventName, preventDefaults, false);
-  document.body.addEventListener(eventName, preventDefaults, false);
 });
-
-// Highlight drop area when item is dragged over it
-["dragenter", "dragover"].forEach((eventName) => {
-  dropArea.addEventListener(eventName, highlight, false);
-});
-["dragleave", "drop"].forEach((eventName) => {
-  dropArea.addEventListener(eventName, unhighlight, false);
-});
-
-// Handle dropped files
-dropArea.addEventListener("drop", handleDrop, false);
 
 function preventDefaults(e) {
   e.preventDefault();
   e.stopPropagation();
 }
 
+["dragenter", "dragover"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, highlight);
+});
+["dragleave", "drop"].forEach((eventName) => {
+  dropArea.addEventListener(eventName, unhighlight);
+});
+
 function highlight(e) {
   dropArea.classList.add("highlight");
 }
 
 function unhighlight(e) {
-  dropArea.classList.remove("active");
+  dropArea.classList.remove("highlight");
 }
 
+dropArea.addEventListener("drop", handleDrop);
+
 function handleDrop(e) {
-  var dt = e.dataTransfer;
-  var files = dt.files;
+  let dt = e.dataTransfer;
+  let files = dt.files;
 
   handleFiles(files);
 }
 
-let uploadProgress = [];
-let progressBar = document.getElementById("progress-bar");
-
-function initializeProgress(numFiles) {
-  progressBar.value = 0;
-  uploadProgress = [];
-
-  for (let i = numFiles; i > 0; i--) {
-    uploadProgress.push(0);
-  }
-}
-
-function updateProgress(fileNumber, percent) {
-  uploadProgress[fileNumber] = percent;
-  let total =
-    uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length;
-  console.debug("update", fileNumber, percent, total);
-  progressBar.value = total;
-}
-
+//without previewFile
 function handleFiles(files) {
-  files = [...files];
-  initializeProgress(files.length);
-  files.forEach(uploadFile);
-  files.forEach(previewFile);
+  [...files].forEach(uploadFile);
 }
 
-function previewFile(file) {
+function uploadFile(file) {
+  let textFromFile = "";
+
   let reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onloadend = function () {
-    let img = document.createElement("img");
-    img.src = reader.result;
-    document.getElementById("gallery").appendChild(img);
+
+  reader.readAsText(file);
+  //async problem code executes while file loading so when the console tries
+  //to log the text from file it was not completed yet
+  //Thus write into onload function
+  console.log("hi 1");
+  reader.onload = function () {
+    textFromFile = reader.result;
+    console.log(typeof reader.result, reader.result);
+    console.log(textFromFile);
+    let paragraph = document.getElementById("textFromFiles");
+
+    paragraph.textContent = textFromFile;
   };
-}
 
-function uploadFile(file, i) {
-  var url = "https://api.cloudinary.com/v1_1/joezimim007/image/upload";
-  var xhr = new XMLHttpRequest();
-  var formData = new FormData();
-  xhr.open("POST", url, true);
-  xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+  reader.onerror = function () {
+    console.log(reader.error);
+  };
 
-  // Update progress (can be used to show progress indicator)
-  xhr.upload.addEventListener("progress", function (e) {
-    updateProgress(i, (e.loaded * 100.0) / e.total || 100);
-  });
-
-  xhr.addEventListener("readystatechange", function (e) {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-      updateProgress(i, 100); // <- Add this
-    } else if (xhr.readyState == 4 && xhr.status != 200) {
-      // Error. Inform the user
-    }
-  });
-
-  formData.append("upload_preset", "ujpu6gyk");
-  formData.append("file", file);
-  xhr.send(formData);
+  console.log("hi 2");
 }
