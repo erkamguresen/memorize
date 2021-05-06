06b16cc8d4ea189da3c2e29ac54d08b50db58393
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-06 20:18:13 +0200

    main panel setup

--- javascript/study.js
+++ javascript/study.js
@@ -13,6 +13,9 @@ function study() {
   //hide aside
   hideAside();
 
+  //setup main panel
+  setupMainPanel();
+
   //load flash cards to study
   loadFlashCards(dataSet);
 }
@@ -20,11 +23,31 @@ function study() {
 function hideAside() {
   const aside = document.querySelector("aside");
   aside.style.display = "none";
+}
 
+function setupMainPanel() {
   const mainTag = document.querySelector("main");
-  mainTag.innerHTML = "<h1>Study</h1>";
 
+  //remove the left border
   const mainDiv = document.querySelector("#main-container");
   mainDiv.className = "";
   mainTag.style.border = "none";
+
+  //empty main panel
+  mainTag.innerHTML = "";
+
+  const display = document.createElement("div");
+  display.setAttribute("id", "display");
+
+  const h1 = document.createElement("h1");
+  h1.textContent = "Study";
+  display.appendChild(h1);
+
+  const studyDiv = document.createElement("div");
+  studyDiv.setAttribute("id", "study");
+  display.appendChild(studyDiv);
+
+  mainTag.appendChild(display);
 }
+
+function loadFlashCards(dataSet) {}
